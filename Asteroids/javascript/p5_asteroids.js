e4f1bb15897f90475aba6f1490ca26368e034d9a
AlexMedoff <alexfmedoff@gmail.com>
2017-11-23 11:25:27 -0500

    Beggining

---
+++ Asteroids/javascript/p5_asteroids.js
@@ -0,0 +1,63 @@
+var keys = [0, 0, 0];		//Array of values to be entered into movement functions
+
+function setup() {
+	createCanvas(600, 600);		//Creates Canvas
+	var ship = new ship();
+}
+
+function draw() {
+	background(20);		//Background color on grayscale
+
+	ship.display();
+
+}
+
+function keyPressed() {
+	if(keyCode == UP_ARROW) {
+		keys[0] = 0;
+	}
+	if(keyCode == RIGHT_ARROW) {
+		keys[1] = 10;
+	}
+	if(keyCode == LEFT_ARROW) {
+		keys[2] = 10;
+	}
+}
+
+function keyReleased() {
+	if(keyCode == UP_ARROW) {
+		keys[0] = 0;
+	}
+	if(keyCode == RIGHT_ARROW) {
+		keys[1] = 0;
+	}
+	if(keyCode == LEFT_ARROW) {
+		keys[2] = 0;
+	}
+}
+
+function toRad(angle) {
+	return angle * (Math.PI/180);
+}
+
+function ship() {
+	this.pos = createVector(width/2, height/2);		//Vector for center of traingle
+	this.radius = 5;
+	this.angle = 90;
+
+	this.turn = function(a) {
+		rotate(a);
+	}
+
+	this.display = function() {
+		translate(this.pos.x, this.pos.y);
+		nofill();
+		stroke(255);
+		triangle(-this.radius, this.radius, this.radius, -this.radius, 0, this,radius);
+	}
+	
+}
+
+function asteroids() {
+
+}
